c964159e5cc27f183b31d781f84b6f639ece778d
Juan Cruz <juancruzroussilian9@gmail.com>
2022-12-18 22:51:51 -0300

    Add option to let user select language for summary and exception handling

--- src/hyp_mod_python/console.py
+++ src/hyp_mod_python/console.py
@@ -6,19 +6,25 @@ import requests
 from . import __version__
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
 
 @click.command()
+@click.option('--lang', default="en", help='Language desired for the summary.')
 @click.version_option(version=__version__)
-def main():
+def main(lang):
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    url = "https://{}.wikipedia.org/api/rest_v1/page/random/summary".format(lang)
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            data = response.json()
+            
+    except:
+        click.secho("An error ocurred while trying to access wikipedia API. Check if the language characters are valid", fg="red")
+        return
 
     title = data["title"]
     extract = data["extract"]
 
     click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
+    
